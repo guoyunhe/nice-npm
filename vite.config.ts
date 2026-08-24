2e0c9aba6d1ef3b10a5f5bea9df6732cbdc853f5
Guo Yunhe <i@guoyunhe.me>
2026-08-24 23:13:01 +0800

    init extension

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
+import { resolve } from 'path';
+
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
+    },
+  },
+  input: {
+    background: resolve(import.meta.dirname, 'src/background/index.ts'),
+    content: resolve(import.meta.dirname, 'src/content/index.ts'),
+    options: resolve(import.meta.dirname, 'options.html'),
+    popup: resolve(import.meta.dirname, 'popup.html'),
+  },
   plugins: [preact()],
-})
+});
